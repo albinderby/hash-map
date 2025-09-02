dd139637f1f0751674e3e4b937708dd8b909aade
albin derby <derbyalbin@gmail.com>
2025-09-02 08:06:24 +0530

    keys and values method added

--- hashmap.js
+++ hashmap.js
@@ -5,7 +5,7 @@ class HASHMAP {
     capcacity = 16;
     filledBuckets = 0;
     bucket = new Array(this.capcacity);
-
+    length = 0;
 
     set(key, value, skipCapacityCheck = false) {
         const hash = this.#hash(key);
@@ -14,12 +14,14 @@ class HASHMAP {
             linkedlist.append({ key, value });
             this.bucket[hash] = linkedlist;
             this.filledBuckets++;
+            this.length++;
         } else {
             const node = this.bucket[hash].find(key);
             if (node) {
                 node.data.value = value;
             } else {
                 this.bucket[hash].prepend({ key, value });
+                this.length++;
             }
         }
         if (!skipCapacityCheck) {
@@ -31,24 +33,56 @@ class HASHMAP {
 
     get(key) {
         const hash = this.#hash(key);
-        if(!this.bucket[hash])return null;
+        if (!this.bucket[hash]) return null;
         const result = this.bucket[hash].find(key);
         return result ? result.data.value : null;
     }
 
-    has(key){
-         const hash = this.#hash(key);
-        return this.bucket[hash]?!!this.bucket[hash].find(key):false;
+    has(key) {
+        const hash = this.#hash(key);
+        return this.bucket[hash] ? !!this.bucket[hash].find(key) : false;
     }
 
-    remove(key){
-        const hash=this.#hash(key);
-        const index=this.bucket[hash].findIndex(key);
-        if(index!==null){
+    remove(key) {
+        const hash = this.#hash(key);
+        const index = this.bucket[hash].findIndex(key);
+        if (index !== null) {
             this.bucket[hash].removeAt(index);
+            this.length--;
             return true;
         }
         return false;
+    }
+    getLength() {
+        return this.length;
+    }
+    clear() {
+        this.capcacity = 16;
+        this.filledBuckets = 0;
+        this.bucket = new Array(this.capcacity);
+        this.length = 0;
+    }   
+    keys(){
+        const array=[];
+       for (const element of this.bucket){
+            let pointer=element.getHead();
+            while(pointer){
+                array.push(pointer.data.key);
+                pointer=pointer.next;
+            }
+        }
+        return array;
+    }
+    values(){
+         const array=[];
+       for (const element of this.bucket){
+            let pointer=element.getHead();
+            while(pointer){
+                array.push(pointer.data.value);
+                pointer=pointer.next;
+            }
+        }
+        return array;
     }
     #hash(key) {
         let hashCode = 0;
@@ -70,6 +104,7 @@ class HASHMAP {
 
     #rehash() {
         const oldbucket = this.bucket;
+        this.length = 0;
         this.bucket = new Array(this.capcacity);
         oldbucket.forEach((list) => {
             if (!list) return;
